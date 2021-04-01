454ab629594853eb466afcbb3f6648c219ca10ee
Hoon <shoh@help-me.kr>
2021-04-01 18:45:47 +0900

    add state to App and update styling

--- music-player-app/src/App.js
+++ music-player-app/src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
 import "./styles/App.scss";
 
 import MusicPlayer from "./components/MusicPlayer";
 import Song from "./components/Song";
 
+import musicData from "./musicData";
+
 function App() {
+  const [songs, setSongs] = useState(musicData());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song></Song>
-      <MusicPlayer></MusicPlayer>
+      <Song currentSong={currentSong} />
+      <MusicPlayer />
     </div>
   );
 }
