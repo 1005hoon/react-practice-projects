da09d229b6259391173831e482e30b48c3992a5f
Hoon Oh <shoh@help-me.kr>
2021-04-03 07:02:27 +0900

    add styling using styled components

--- photo-company-app/src/App.tsx
+++ photo-company-app/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 
 function App(): JSX.Element {
   return (
     <>
+      <GlobalStyle />
       <AboutUs />
     </>
   );
