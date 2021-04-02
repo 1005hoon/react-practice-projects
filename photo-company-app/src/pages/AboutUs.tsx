c2943a908d1f3a2e2ad58a25869def4400add51e
Hoon Oh <shoh@help-me.kr>
2021-04-03 06:39:14 +0900

    add faq section

--- photo-company-app/src/pages/AboutUs.tsx
+++ photo-company-app/src/pages/AboutUs.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = (): JSX.Element => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
