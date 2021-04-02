5e3f29de028c7fd73e60739b86e33e36145d4eea
Hoon <shoh@help-me.kr>
2021-04-02 15:43:01 +0900

    class component with interface

--- react-with-typescript/src/index.tsx
+++ react-with-typescript/src/index.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-  return <h1>hello world</h1>;
+interface AppProps {
+  color?: string;
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  onAdd = (): void => this.setState({ counter: this.state.counter + 1 });
+  onSubtract = (): void => this.setState({ counter: this.state.counter - 1 });
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.onAdd}>Add</button>
+        <button onClick={this.onSubtract}>Minus</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App color="red" />, document.querySelector("#root"));
