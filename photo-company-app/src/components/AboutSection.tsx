c654aa12e2cb7d823975b2cdc5d72c32e695058a
Hoon <shoh@help-me.kr>
2021-04-02 18:45:51 +0900

    add services section

--- photo-company-app/src/components/AboutSection.tsx
+++ photo-company-app/src/components/AboutSection.tsx
@@ -3,7 +3,7 @@ import home1 from "../img/home1.png";
 
 const AboutSection = (): JSX.Element => {
   return (
-    <div>
+    <>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -27,7 +27,7 @@ const AboutSection = (): JSX.Element => {
       <div className="image">
         <img src={home1} alt="guy with a camera" />
       </div>
-    </div>
+    </>
   );
 };
 
--- photo-company-app/src/pages/AboutUs.tsx
+++ photo-company-app/src/pages/AboutUs.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = (): JSX.Element => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+    </>
   );
 };
 
