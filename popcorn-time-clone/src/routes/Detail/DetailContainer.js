ea93b1c954e4a483437f16999eecc66aa0eccf31
Hoon <shoh@help-me.kr>
2021-04-05 17:14:59 +0900

    update detailed components

--- popcorn-time-clone/src/routes/Detail/DetailContainer.js
+++ popcorn-time-clone/src/routes/Detail/DetailContainer.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { movieAPI, tvAPI } from "../../api";
 import DetailPresenter from "./DetailPresenter";
 
 export class DetailContainer extends Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -16,8 +25,27 @@ export class DetailContainer extends Component {
       history: { push },
     } = this.props;
 
+    const { isMovie } = this.state;
+
     if (isNaN(+id)) {
       return push("/");
+    }
+
+    let result = null;
+
+    this.setState({ loading: true });
+    try {
+      if (isMovie) {
+        const response = await movieAPI.movieDetailsById(+id);
+        result = response.data;
+      } else {
+        const response = await tvAPI.TVDetailsById(+id);
+        result = response.data;
+      }
+    } catch (error) {
+      this.setState({ error: "failed to fetch movie data" });
+    } finally {
+      this.setState({ loading: false });
     }
   }
 
