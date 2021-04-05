b078b4fc0ddc967d327563c84ecc5ddc15b25bca
Hoon <shoh@help-me.kr>
2021-04-05 11:16:10 +0900

    add basic routing to project

---
+++ popcorn-time-clone/src/components/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as AppRouter,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Home from "../routes/Home";
+import TV from "../routes/TV";
+import Search from "../routes/Search";
+
+const Router = () => {
+  return (
+    <AppRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/TV" exact component={TV} />
+        <Route path="/TV/popular" render={() => <h1>popular</h1>} />
+        <Route path="/Search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </AppRouter>
+  );
+};
+
+export default Router;
---
+++ popcorn-time-clone/src/routes/TV.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TV = () => {
+  return (
+    <div>
+      <h1>TV page</h1>
+    </div>
+  );
+};
+
+export default TV;
