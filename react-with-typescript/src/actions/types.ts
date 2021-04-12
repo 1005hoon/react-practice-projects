6221cc8e1a6c20ba5d8082212e4dd82387c2857c
Hoon <shoh@help-me.kr>
2021-04-13 08:45:43 +0900

    remove not using project

--- react-with-typescript/src/actions/types.ts
+++
@@ -1,8 +0,0 @@
-import { FetchTodosAction, DeleteTodosAction } from "./todos";
-
-export enum ActionTypes {
-  fetchTodos,
-  deleteTodo,
-}
-
-export type Action = FetchTodosAction | DeleteTodosAction;
