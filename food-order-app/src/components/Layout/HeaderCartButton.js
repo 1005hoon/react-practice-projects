e1c0c1d457b6b06aa6a8c7a5e83103f535653555
Hoon <shoh@help-me.kr>
2021-04-15 15:32:37 +0900

    add tiny bump animation

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cartContext";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const { items } = useContext(CartContext);
+  const [btnHighlighed, setBtnHighlighted] = useState(false);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+
+    const timerId = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return () => clearTimeout(timerId);
+  }, [items]);
 
   const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button
+      className={`${btnHighlighed ? classes.bump : ""} ${classes.button}`}
+      onClick={props.onClick}
+    >
       <span className={classes.icon}>
         <CartIcon />
       </span>
