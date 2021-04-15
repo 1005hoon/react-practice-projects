8176972b4975469a860d252ba4a508530d821f31
Hoon <shoh@help-me.kr>
2021-04-15 14:36:23 +0900

    add outputting cart items logic

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cartContext";
+import CartItem from "./CartItem";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const { items, totalAmount } = useContext(CartContext);
+
+  const formattedAmount = `$${totalAmount.toFixed(2)}`;
+  const hasItems = items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: " Some Food", amount: 2, price: 12.99 }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={() => cartItemRemoveHandler(item.id)}
+          onAdd={() => cartItemAddHandler(item)}
+        />
+      ))}
     </ul>
   );
+
   return (
     <Modal onClick={props.onCloseCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{formattedAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onCloseCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
