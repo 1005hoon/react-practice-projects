22a78bd65aa1cf02906a41e5db52dfeb34015247
Hoon <shoh@help-me.kr>
2021-04-15 10:27:02 +0900

    add meals component

---
+++ food-order-app/src/components/Meals/MealsSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto animi
+        facilis harum repudiandae. Debitis error vel, assumenda laudantium
+        dolorem officiis?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto animi
+        facilis harum repudiandae. Debitis error vel, assumenda laudantium
+        dolorem officiis?
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
