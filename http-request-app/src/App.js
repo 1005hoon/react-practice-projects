b45e8076831dc3a3536c68f77cce398785fd51c7
Hoon <shoh@help-me.kr>
2021-04-19 14:56:16 +0900

    use useeffect and usecallback for fetching data on app mounting

--- http-request-app/src/App.js
+++ http-request-app/src/App.js
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import MoviesList from "./components/MoviesList";
 
 const App = () => {
-  const dummyData = [
-    {
-      id: 1,
-      title: "아무 영화나 넣어보세요",
-      description: "이게 영화 설명이 들어가는 칸입니다",
-      releaseDate: "2021-05-22",
-    },
-    {
-      id: 2,
-      title: "또 다른 영화를 넣어보세요",
-      description: "이게 영화 설명이 들어가는 칸입니다",
-      releaseDate: "2021-10-05",
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const fetchMovieHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        throw new Error("에러가 발생했습니다");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          description: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
+
+  let renderContent = <p>데이터가 없습니다</p>;
+
+  if (movies.length > 0) {
+    renderContent = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    renderContent = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    renderContent = <h1>데이터를 로딩중입니다</h1>;
+  }
   return (
     <>
       <section>
-        <button>영화정보 가져오기</button>
+        <button onClick={fetchMovieHandler}>영화정보 가져오기</button>
       </section>
-      <section>
-        <MoviesList movies={dummyData} />
-      </section>
+      <section>{renderContent}</section>
     </>
   );
 };
